6fa436cdcdc5b6fa8704be32079fab9d309fc981
VinayakTheDev <vinayakdeshpande11@gmail.com>
2022-07-11 23:55:58 +0530

    Update init_db.sh

--- init_db/postgres/init_db.sh
+++ init_db/postgres/init_db.sh
@@ -4,8 +4,8 @@ set -eux
 THISDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 #Demo PostgreSQL Database initialisation
-psql postgres -c "CREATE USER testuser PASSWORD 'testpass'"
+psql postgres -c "CREATE USER thor PASSWORD 'thorofasgard'"
 #The -O flag below sets the user: createdb -O DBUSER DBNAME
-createdb -O testuser testdb
+createdb -O thor datagod
 
-psql -d testdb -U testuser -f $THISDIR/seed_db.sql
+psql -d datagod -U thor -f $THISDIR/seed_db.sql
